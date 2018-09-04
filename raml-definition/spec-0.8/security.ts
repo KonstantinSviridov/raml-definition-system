8745a29e6aa8102b10d76a0f8c96c77f92e6b882
KonstantinSviridov <ksviridov@gmail.com>
2018-09-05 02:48:54 +0700

    authorixation grants enum for RAML 0.8

--- raml-definition/spec-0.8/security.ts
+++ raml-definition/spec-0.8/security.ts
@@ -168,6 +168,7 @@ export class OAuth2SecuritySchemeSettings extends  SecuritySchemeSettings {
 
     authorizationGrants:string[]
     $authorizationGrants = [
+        MetaModel.oftenKeys(["code", "token", "owner", "credentials"]),
         MetaModel.description("A list of the Authorization grants supported by the API as defined in RFC6749 Sections 4.1, 4.2, 4.3 " +
             "and 4.4, can be any of: authorization_code, password, client_credentials, implicit, or refresh_token."),
         MetaModel.markdownDescription("A list of the Authorization grants supported by the API as defined in RFC6749 " +
